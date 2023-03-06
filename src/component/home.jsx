4c62e3df9f6dac4278d637de1f04bbee3ebd9234
Kamilia <kamilia.mimouni@polytechnicien.tn>
2023-03-06 16:22:37 +0100

    crudReactCRA

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 
 function Home() {
-    const [msg]=useState('welcome to our company website: KMK-AI')
+    const [msg]=useState('welcome to KMK')
     
 
     return ( 
